34aaaf3d668e5db0119e7de504206f1ae19fdb0e
avaljotsingh <32036444+avaljotsingh@users.noreply.github.com>
2023-03-23 17:16:31 -0500

    Update test_input.py

--- python/test_input.py
+++ python/test_input.py
@@ -1,13 +1,11 @@
 def Shape as (Float l, Float u, PolyExp L, PolyExp U){(curr[l]<=curr) and (curr[L]<=curr) and (curr[u]>=curr) and (curr[U]>=curr)}
 
-func simplify_lower(Neuron n, Float c) = (c>=0) ? (c*n[l]) : (c*n[u])
-func simplify_upper(Neuron n, Float c) = (c>=0) ? (c*n[u]) : (c*n[l])
-func replace_lower(Neuron n, Float c) = (c>=0) ? (c*n[L]) : (c*n[U])
-func replace_upper(Neuron n, Float c) = (c>=0) ? (c*n[U]) : (c*n[L])
-func backsubs_lower(PolyExp e, Neuron n) = (e.traverse(backward, priority1, true, replace_lower){e<=n}).map(simplify_lower)
-func backsubs_upper(PolyExp e, Neuron n) = (e.traverse(backward, priority1, true, replace_upper){e>=n}).map(simplify_upper)
-func priority1(Neuron n) = -n[layer]
-func priority2(Neuron n) = n[layer]
+func simplify_lower(Neuron n, Float c) = (c>=0) ? (c*n[l]) : (c*n[u]);
+func simplify_upper(Neuron n, Float c) = (c>=0) ? (c*n[u]) : (c*n[l]);
+func replace_lower(Neuron n, Float c) = (c>=0) ? (c*n[L]) : (c*n[U]);
+func replace_upper(Neuron n, Float c) = (c>=0) ? (c*n[U]) : (c*n[L]);
+func backsubs_lower(PolyExp e, Neuron n) = (e.traverse(backward, -n[layer], true, replace_lower){e<=n}).map(simplify_lower);
+func backsubs_upper(PolyExp e, Neuron n) = (e.traverse(backward, -n[layer], true, replace_upper){e>=n}).map(simplify_upper);
 
 transformer deeppoly(curr, prev){
     Relu -> sum(prev[l])>=0 ? (sum(prev[l]), sum(prev[u]), sum(prev[L]), sum(prev[U])) : 
@@ -17,7 +15,7 @@ transformer deeppoly(curr, prev){
     Affine -> (backsubs_lower(prev.dot(curr[weight]) + curr[bias], curr), backsubs_upper(prev.dot(curr[weight]) + curr[bias], curr), 
     prev.dot(curr[weight]) + curr[bias], prev.dot(curr[weight]) + curr[bias])
 }
-flow(forward, priority2, true, deeppoly)
+flow(forward, n[layer], true, deeppoly)
 
 
 
