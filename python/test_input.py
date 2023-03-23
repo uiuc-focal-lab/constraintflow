585e50508ffd1a57885cb38ce2a62278e61610d9
avaljotsingh <32036444+avaljotsingh@users.noreply.github.com>
2023-03-23 17:14:57 -0500

    Update test_input.py

--- python/test_input.py
+++ python/test_input.py
@@ -3,9 +3,7 @@ def Shape as (Float l, Float u, PolyExp L, PolyExp U){(curr[l]<=curr) and (curr[
 func simplify_lower(Neuron n, Float c) = (c>=0) ? (c*n[l]) : (c*n[u])
 func simplify_upper(Neuron n, Float c) = (c>=0) ? (c*n[u]) : (c*n[l])
 func replace_lower(Neuron n, Float c) = (c>=0) ? (c*n[L]) : (c*n[U])
-func replace_upper(Neuron n, Float c) = (c>=0) ? (c*n[U]) : (c*n[L]) 
-func priority1(Neuron n) = -n[layer]
-func priority2(Neuron n) = n[layer]
+func replace_upper(Neuron n, Float c) = (c>=0) ? (c*n[U]) : (c*n[L])
 func backsubs_lower(PolyExp e, Neuron n) = (e.traverse(backward, priority1, true, replace_lower){e<=n}).map(simplify_lower)
 func backsubs_upper(PolyExp e, Neuron n) = (e.traverse(backward, priority1, true, replace_upper){e>=n}).map(simplify_upper)
 
